aea89be04c26d507f50ca9434152777c9cb7dff2
joshvito <josh.vito@gmail.com>
2015-12-08 19:26:14 -0500

    7z old archive files; update sftp config;

--- archived/coreipgs/header_index.js
+++
@@ -1,9 +0,0 @@
-
-<!-- Start
-
-// NOTE: If you use a ' add a slash before it like this \'
-
-document.write('<img src="picts/logo.jpg" HEIGHT="167" WIDTH="400" border="0"><br>');
-
-
-//  End -->
--- archived/coreipgs/javascripts.js
+++
@@ -1,8 +0,0 @@
-<!--
-
-
-function blockError(){return true;}
-window.onerror = blockError;
-
-
-// -->
